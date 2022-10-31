2e0fddce2e6c879c3e7399616aab32fb255cf09e
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-10-31 21:22:21 +0200

    commented out passport.ts

---
+++ api/src/config/passport.ts
@@ -0,0 +1,13 @@
+import passport from 'passport'
+// import JwtStrategy, { ExtractJwt } from 'passport-jwt';
+
+// export const jwtStrategy = new JwtStrategy.Strategy(
+//     {
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       secretOrKey: `${process.env.JWT_SECRET}`,
+//     },
+//     (jwt_payload, done) => {
+//       const user = jwt_payload
+//       done(null, user)
+//     }
+//   )
